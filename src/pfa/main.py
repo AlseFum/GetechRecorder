69650ae60be536c5497f728aabc9f915cc172f74
胡龄 <2652328248@qq.com>
2025-03-15 19:59:18 +0800

    finished basic process:added endpoint '/debug/register' and '/debug/login' to simplify development and as usage reference.

--- src/pfa/main.py
+++ src/pfa/main.py
@@ -1,23 +1,47 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,Request
+from pydantic import BaseModel
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
-    return {"message": "Hello, World!"}
 @app.get("/nihao")
-async def nihao():
-    return {"message": "nihao"}
+async def read_root():
+    return {"message": "Hello, Getech!"}
 
-from .some import public_key_pem
+
+from .cryption import public_key_pem,private_key,server_register,server_login,client_encrypt_data
+
 @app.get("/api/login/publickey")
 async def get_public_key():
     return {"public_key": public_key_pem}
 
+class Item(BaseModel):
+    name: str
+    key:str
+@app.post("/debug/register")
+async def dregister(item:Item):
+    (name,key)=item
+    _,name=name
+    _,key=key
+    return {"message":server_register(private_key,name,client_encrypt_data(public_key_pem,key))}
+@app.post("/debug/login")
+async def dlogin(item:Item):
+    (name,key)=item
+    _,name=name
+    _,key=key
+    return {"message":server_login(private_key,name,client_encrypt_data(public_key_pem,key))} 
+
 @app.post("/api/login/register")
-async def register():
-    return {"message": "注册成功"}
+# 定义一个异步函数 register，用于注册某个项目
+async def register(item:Item):
+    # 从传入的 item 对象中解包出 name 和 ek 两个变量
+    (name,key)=item
+    _,name=name
+    _,key=key
+    return {"message":server_register(private_key,name,key)}
 
 @app.post("/api/login/login")
-async def bur():
-    return {"message": "登录成功"}
+async def bur(item:Item):
+    (name,key)=item
+    _,name=name
+    _,key=key
+    return {"message":server_login(private_key,name,key)}
